39693cabc8e851074c23fd0b4fe3dbe23d8b3158
L <1574033296@qq.com>
2020-03-22 17:10:41 +0800

    优化邮件功能，template使用文件加载

--- src/main/java/com/nowcoder/wenda/util/MailSender.java
+++ src/main/java/com/nowcoder/wenda/util/MailSender.java
@@ -1,12 +1,18 @@
 package com.nowcoder.wenda.util;
 
 import com.sun.mail.util.MailSSLSocketFactory;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import sun.security.krb5.Config;
+
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
@@ -21,14 +27,16 @@ public class MailSender implements InitializingBean {
     private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
     private JavaMailSenderImpl mailSender;
 
+    @Autowired
+    Configuration configuration;
 //    @Autowired
 //    private VelocityEngine velocityEngine;
 
     public boolean sendWithHTMLTemplate(String to, String subject,
-                                        String templets, Map<String, Object> model) {
+                                        String template, Map<String, Object> model) {
         try {
             String nick = MimeUtility.encodeText("登录");
-            InternetAddress from = new InternetAddress(nick + "<xx@qq.com>");
+            InternetAddress from = new InternetAddress(nick + "<1574033296@qq.com>");
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
 
@@ -36,12 +44,14 @@ public class MailSender implements InitializingBean {
             sf.setTrustAllHosts(true);
 
 
-//            String result = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, template, "UTF-8", model);
+            Template tpl = configuration.getTemplate(template);     //free marker configuration
+            String result = FreeMarkerTemplateUtils.processTemplateIntoString(tpl, model);      //freemarker 加载template
+//            String result = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, template, "UTF-8", model);        //velocity加载template
             mimeMessageHelper.setTo(to);    //发给那个邮件
             mimeMessageHelper.setFrom(from);    //从什么地方发
             mimeMessageHelper.setSubject(subject);  //邮件主题
-//            mimeMessageHelper.setText(result, true);
-            mimeMessageHelper.setText("hello, this message from mailSender");
+            mimeMessageHelper.setText(result, true);
+//            mimeMessageHelper.setText("hello, this message from mailSender");
             mailSender.send(mimeMessage);
             return true;
         } catch (Exception e) {
@@ -53,8 +63,8 @@ public class MailSender implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         mailSender = new JavaMailSenderImpl();
-        mailSender.setUsername("xxx@qq.com");
-        mailSender.setPassword("xxxx");     //邮箱授权码，账号密码修改会使该码过期
+        mailSender.setUsername("1574033296@qq.com");
+        mailSender.setPassword("rvoadgwvweazibgd");     //邮箱授权码，账号密码修改会使该码过期
 //        mailSender.setHost("smtp.exmail.qq.com");
         mailSender.setHost("smtp.qq.com");
         mailSender.setPort(465);
